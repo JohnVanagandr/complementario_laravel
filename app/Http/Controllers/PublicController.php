a060484ef9a29b5283f8528c408769b38fc632ea
JohnVanagandr <jfbeccera@gmail.com>
2024-08-19 03:56:15 -0500

    Actididad, finalizar la relacion entre los post, tags y las categorias para las vistas publicas

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
@@ -9,8 +10,11 @@ class PublicController extends Controller
 {
   function index()
   {
+    // Listamos los posts de todos los usuarios
     $posts = Post::paginate(15);
+    // Listamos las categorias
+    $categories = Category::all();
 
-    return view('index', compact('posts'));
+    return view('index', compact('posts', 'categories'));
   }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,47 +3,71 @@
     @section('content')
         <div class="container">
             <div class="row">
-                @foreach ($posts as $post)
-                    <div class="col-md-4">
-                        <div class="card">
+                <div class="col-md-9">
+                    <div class="row">
+                        @foreach ($posts as $post)
+                            <div class="col-md-4">
+                                <div class="card">
 
-                            {{-- Carrousel --}}
-                            <div id="carouselExample_{{ $post->id }}" class="carousel slide" data-bs-ride="carousel">
-                                <div class="carousel-inner">
-                                    @foreach ($post->images as $item)
-                                        @if ($loop->first)
-                                            <div class="carousel-item active">
-                                                <img src="{{ $item->url_path }}" class="d-block w-100"
-                                                    alt="{{ $item->name }}">
-                                            </div>
-                                        @else
-                                            <div class="carousel-item">
-                                                <img src="{{ $item->url_path }}" class="d-block w-100"
-                                                    alt="{{ $item->name }}">
-                                            </div>
-                                        @endif
-                                    @endforeach
+                                    {{-- Carrousel --}}
+                                    <div id="carouselExample_{{ $post->id }}" class="carousel slide"
+                                        data-bs-ride="carousel">
+                                        <div class="carousel-inner">
+                                            @foreach ($post->images as $item)
+                                                @if ($loop->first)
+                                                    <div class="carousel-item active">
+                                                        <img src="{{ $item->url_path }}" class="d-block w-100"
+                                                            alt="{{ $item->name }}">
+                                                    </div>
+                                                @else
+                                                    <div class="carousel-item">
+                                                        <img src="{{ $item->url_path }}" class="d-block w-100"
+                                                            alt="{{ $item->name }}">
+                                                    </div>
+                                                @endif
+                                            @endforeach
+                                        </div>
+                                        <button class="carousel-control-prev" type="button"
+                                            data-bs-target="#carouselExample_{{ $post->id }}" data-bs-slide="prev">
+                                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                            <span class="visually-hidden">Previous</span>
+                                        </button>
+                                        <button class="carousel-control-next" type="button"
+                                            data-bs-target="#carouselExample_{{ $post->id }}" data-bs-slide="next">
+                                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                            <span class="visually-hidden">Next</span>
+                                        </button>
+                                    </div>
+
+
+                                    <div class="card-body">
+                                        <h5 class="card-title">{{ $post->title }}</h5>
+                                        <p class="card-text">{{ $post->body }}</p>
+                                    </div>
+
+                                    <div class="card-footer bg-transparent border-success">
+                                        @foreach ($post->tags as $tag)
+                                            <a href="#" class="btn btn-light">{{ $tag->name }}</a>
+                                        @endforeach
+                                    </div>
                                 </div>
-                                <button class="carousel-control-prev" type="button"
-                                    data-bs-target="#carouselExample_{{ $post->id }}" data-bs-slide="prev">
-                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                    <span class="visually-hidden">Previous</span>
-                                </button>
-                                <button class="carousel-control-next" type="button"
-                                    data-bs-target="#carouselExample_{{ $post->id }}" data-bs-slide="next">
-                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                    <span class="visually-hidden">Next</span>
-                                </button>
                             </div>
-
-
-                            <div class="card-body">
-                                <h5 class="card-title">{{ $post->title }}</h5>
-                                <p class="card-text">{{ $post->body }}</p>
-                            </div>
-                        </div>
+                        @endforeach
                     </div>
-                @endforeach
+                </div>
+                <div class="col-md-3">
+                    <div class="card">
+                        <ul class="list-group list-group-flush navbar-nav">
+                            @foreach ($categories as $category)
+                                <li class="list-group-item nav-item">
+                                    <a href="#" class="nav-link">
+                                        {{ $category->name }}
+                                    </a>
+                                </li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
     @endsection
